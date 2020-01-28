317b720671fff1f08179badf2e4e7b0457ce68db
Carla Geglio <carla.geglio@gmail.com>
2020-01-27 19:25:14 -0700

    Refactor newsarticle

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './NewsArticle.css';
 
 const NewsArticle = (props) => {
-  return <div className={props.id}><img src={props.image} alt=''/><h1>{props.headline}</h1><p>{props.description}</p><a href={props.url}>Link To Article</a></div>;
+  return (
+    <div className="article">
+      <img src={props.image} alt=''/>
+      <h1>{props.headline}</h1>
+      <p>{props.description}</p>
+      <a href={props.url}>Link To Article</a>
+    </div>
+  )
 }
 
 
