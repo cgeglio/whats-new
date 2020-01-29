89e3f54372991dee4365d4f39975ea8e5a0eb8cf
Carla Geglio <carla.geglio@gmail.com>
2020-01-28 18:26:32 -0700

    Add logo and improve menu styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './Menu.css'
+import './Menu.css';
+import logo from '../../images/whats-new-logo.png'
+
 
 const Menu = (props) => (
   <div className="menu">
-    {props.labels.map(l => <button className="menu-button" key={l} onClick={(event) => props.updateNews(l)}>{l}</button>)}
+    <img src={logo} alt='whats new logo' className="logo-image"/>
+    {props.labels.map(l => <button className="menu-button" key={l} onClick={(event) => props.updateNews(l)}>{l[0].toUpperCase() + l.slice(1)}</button>)}
   </div>
 );
 
