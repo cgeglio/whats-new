e55753b1c4137c453b7fbf9bd9383d7b85468a97
Carla Geglio <carla.geglio@gmail.com>
2020-01-30 13:48:26 -0700

    Add tests for clicks and state

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -13,6 +13,20 @@ describe('App', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
+  it('should start off with a default state', () => {
+    const wrapper = shallow(<App />);
+    const expected = {topic: '', articles: []};
+    expect(wrapper.state()).toEqual(expected);
+  });
+
+  it('should sort news data and update state when sortNews is called', () => {
+    const wrapper = shallow(<App />);
+    const mockData = {"local": [{headline: "article1"}], "health": [{headline: "article2"}]};
+
+    wrapper.instance().componentDidMount();
+    expect(wrapper.state()).toEqual({topic: 'local', articles: [{headline: "article1"}], topic: 'health', articles: [{headline: "article2"}]});
+  });
+
   it('should update state with a topic and articles when updateNews is called', () => {
     const wrapper = shallow(<App />);
     const mockTopic = "health";
