fc353fd52d380f5a8be535338ed2a95f2b088ff1
Carla Geglio <carla.geglio@gmail.com>
2020-01-29 20:12:23 -0700

    Add method tests for search form

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -13,4 +13,41 @@ describe('SearchForm', () => {
     />);
   expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update state with a new keyword when updateKeyword is called', () => {
+    const wrapper = shallow(<SearchForm />);
+    const mockEvent = {target: {name: 'keyword', value: 'denver'}};
+    const expected = {keyword:'denver'};
+
+    expect(wrapper.state()).toEqual({keyword: ''});
+    wrapper.instance().updateKeyword(mockEvent);
+    expect(wrapper.state()).toEqual(expected);
+  });
+
+  it('should find search related articles and call updateArticles when findArticles is called', () => {
+    const wrapper = shallow(<SearchForm
+      newsItems={[
+        {topic: "local", articles: []},
+        {topic: "entertainment", articles: []}
+      ]}
+      updateFromSearch={jest.fn()}
+    />);
+    const mockEvent = {preventDefault: jest.fn()};
+
+    wrapper.instance().updateArticles = jest.fn();
+    wrapper.instance().findArticles(mockEvent);
+    expect(wrapper.instance().updateArticles).toHaveBeenCalled();
+  });
+
+  it('should call updateFromSearch and reset state when updateArticles is called', () => {
+    const wrapper = shallow(<SearchForm
+      updateFromSearch={jest.fn()}
+    />);
+    const mockArticles = [{headline: 'denver'}, {headline: 'colorado'}]
+    const expected = {keyword: ''};
+
+    wrapper.instance().updateArticles(mockArticles);
+    expect(wrapper.instance().props.updateFromSearch).toHaveBeenCalled();
+    expect(wrapper.state()).toEqual(expected);
+  });
 });
